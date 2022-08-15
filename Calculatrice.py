67919ad3572719b43d18ffc197913e7702641cd1
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-08-15 21:48:19 +0300

    calcula2

--- Calculatrice.py
+++ Calculatrice.py
@@ -2,11 +2,11 @@ from tkinter import *
 
 if __name__ == "__main__":
     root = Tk()
-    root.configure(background="#101419")
+    root.configure(background="#984447")
     root.title("Calculatrice By Finoana Randri")
-    root.geometry("385x485")
-    root.minsize(385,485)
-    #root.maxsize(385,485)
+    root.geometry("385x570")
+    root.minsize(385,570)
+    root.maxsize(385,570)
 #ici c'est la variable pour stocker les contenus
     equation = StringVar()
 #boite de resultat
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         if colonne ==4:
             colonne=0
             ligne +=1
-        b= Label(root,text="Effacer",bg="red",fg="#fff",height=5,width=12)
+        b= Label(root,text="Effacer",bg="#984447",fg="#FFF",height=5,width=12)
         b.bind("<Button-1>",print)
         b.grid(columnspan=4)
     root.mainloop()
