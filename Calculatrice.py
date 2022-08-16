aafad3f3ad61288863d50ba200bcf4e5a1d6bf04
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-08-16 10:29:06 +0300

    calcula2

--- Calculatrice.py
+++ Calculatrice.py
@@ -1,5 +1,33 @@
 from tkinter import *
 
+
+expression =""
+
+
+def appuyer(touche):
+     if touche =="=":
+         calculer()
+         return
+     global expression 
+     expression += str(touche)
+     equation.set(expression)
+#ici je defini la fontion calculer ainsi que les try et exept pour que l appli ne plante pas face au calcule impossible
+def calculer():
+    try:
+        global expression
+        total = str(eval(expression))
+        
+        equation.set(total)
+        expression=total
+    except:
+        equation.set("erreur")
+        expression=""    
+
+def effacer():
+    global expression
+    expression =""
+    equation.set("")
+
 if __name__ == "__main__":
     root = Tk()
     root.configure(background="#984447")
@@ -19,7 +47,7 @@ if __name__ == "__main__":
     for bouton in boutons:
         b= Label(root,text=str(bouton),bg="#476C9B",fg="#fff",height=8,width=12)
     #Vision Xd Follow Finoana On Git Hub or Po**Hub xdxd
-        b.bind("<Button-1>",print)
+        b.bind("<Button-1>",lambda e,bouton=bouton: appuyer(bouton))
     
         b.grid(row=ligne,column=colonne)
         
@@ -28,6 +56,6 @@ if __name__ == "__main__":
             colonne=0
             ligne +=1
         b= Label(root,text="Effacer",bg="#984447",fg="#FFF",height=5,width=12)
-        b.bind("<Button-1>",print)
+        b.bind("<Button-1>",lambda e: effacer())
         b.grid(columnspan=4)
     root.mainloop()
